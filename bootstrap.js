e58077c610d4c756d561bfadadc217e1c52985c7
Kasia Kordys <kasia.kordys@gmail.com>
2016-02-20 22:00:29 +0000

    project files added

---
+++ bootstrap.js
@@ -0,0 +1,17 @@
+// When the user submits the form,
+    //   Check what answer they picked
+    //   And tell them if they're correct
+
+    // submit
+    $("#quiz-form").on("submit", function(event) {
+        event.preventDefault();
+        var $answer = $("#quiz-answer"); 
+        var answer = $answer.val();
+        console.log(answer);
+        if (answer === "4 pairs") {
+            $("#result").text("Congratulations! You are expert on jumping spiders!");
+        } else {
+            $("#result").text("Try again!");
+        }
+        
+    });
