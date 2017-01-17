739fbd90a805df3529cdf829f7ae2d0aff9bafbc
Kasia Kordys <kasia.kordys@gmail.com>
2017-01-17 21:19:12 +0000

    Modify quiz result text and style

--- quiz.js
+++ quiz.js
@@ -1,19 +1,24 @@
-// When the user submits the form,
+    // When the user submits the form,
     //   Check what answer they picked
     //   And tell them if they're correct
 
     // submit
+$(document).ready(function() {
+    
     $("#quiz-form").on("submit", function(event) {
         event.preventDefault();
         var $answer = $("#quiz-answer"); 
         var answer = $answer.val();
         console.log(answer);
         if (answer === "4 pairs") {
-            $("#result").text("Congratulations! You are expert on jumping spiders!");
-            $("#result").css("color", "blue");
+            $("#result").text("Congratulations! You are an expert on jumping spiders!");
+            $("#result").css("color", "white");
         } else {
             $("#result").text("Try again!");
-            $("#result").css("color", "red");
+            $("#result").css("color", "black");
         }
         
     });
+    
+});
+        
